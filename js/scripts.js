5f079b1d2db791ba4bc38d3e9513d8a596eb05d1
Derin Serbetcioglu <serbetd@gmail.edu>
2018-07-27 10:53:43 -0700

    dynamic publications and speaker series created and implemented with other mnor changes

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,64 @@
     });
 
 })(jQuery);
+
+
+
+
+
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.move').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (40 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (30 * lMouseY) / 100;
+
+});
+
+moveBackground();
+
+
+
+
+//
+// $(document).ready(function() {
+// var movementStrength = 25;
+// var height = movementStrength / $(window).height();
+// var width = movementStrength / $(window).width();
+// $("#top-image").mousemove(function(e){
+//           var pageX = e.pageX - ($(window).width() / 2);
+//           var pageY = e.pageY - ($(window).height() / 2);
+//           var newvalueX = width * pageX * -1 - 25;
+//           var newvalueY = height * pageY * -1 - 50;
+//           $('#top-image').css("background-position", newvalueX+"px     "+newvalueY+"px");
+// });
+// });
+//
+//
+//
+// $('#landing-content').mousemove(function(e){
+//     var amountMovedX = (e.pageX * -1 / 6);
+//     var amountMovedY = (e.pageY * -1 / 6);
+//     $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+// });
